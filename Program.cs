c275ea4c82475eb808f1d4ccfd4a1f23f35d86c7
ac148838 <ac148838@8CG8163L42>
2025-05-16 09:58:03 +1200

    I'm trying to connect to my database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace vinylApp
+﻿using vinylApp.Repositories;
+
+namespace vinylApp
 {
-    internal class Program
+    public class Program
     {
+        private static StorageManager storageManager;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string connectionString = "Data Source=" +
+                "(localdb)" +
+                "\\MSSQLLocalDB;Initial" +
+                " Catalog=vinylDB;" +
+                "Integrated " +
+                "Security=True;Connect " +
+                "Timeout=30;Encrypt=False;" +
+                "Trust Server Certificate=False;Application " +
+                "Intent=ReadWrite;Multi Subnet Failover=False";
+
+            storageManager= new StorageManager(connectionString);
         }
     }
 }
