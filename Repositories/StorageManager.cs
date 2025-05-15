a5f0a3f6f8effa462f2bbe8d3435603d468a0f10
ac148838 <ac148838@8CG8163L42>
2025-05-16 09:37:33 +1200

    Add project files.

---
+++ Repositories/StorageManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vinylApp.Repositories
+{
+    public class StorageManager
+    {
+        private SqlConnection conn;
+    }
+}
