cfaf6d09ae7a2f5ebad497356e7b06c009dfdc9c
AwesomeGvincent01 <ac148838@avcol.school.nz>
2025-06-28 18:22:07 +1200

    User table + user crud WIP
    To begin with my reigsters/logins, I created a new table in my database called "User", which will be used to store user/admin login information (when the user registers, their user/password gets stored here).
    
    On top of that, I also just started working on my crud for the user table.

---
+++ vinylApp/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vinylApp.vinylApp.Model
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User(int userId, string username, string password, bool isAdmin)
+        {
+            UserID = userId;
+            Username = username;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+    }
+}
